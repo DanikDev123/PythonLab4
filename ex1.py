4043c4d4b8c47c8b70322ce16ce104a77e04c017
213 <107265321+DanikDev123@users.noreply.github.com>
2024-11-21 12:43:05 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -5,9 +5,9 @@ c = int (input("Enter c: "))
 
 D = b**2 - 4*a*c
 
-r1 = (-b + math.sqrt(D))/2*a
-r2 = (-b - math.sqrt(D))/2*a
-r3 = -b/2*a
+r1 = (-b + math.sqrt(D))/(2*a)
+r2 = (-b - math.sqrt(D))/(2*a)
+r3 = -b/(2*a)
 
 if D > 0:
     print("Root 1: ", r1)
