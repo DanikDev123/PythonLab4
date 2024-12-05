34fc707c748b4cf0bcd441d35088bef9fb50528e
213 <107265321+hzDev213@users.noreply.github.com>
2024-12-05 11:06:56 +0300

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,4 +1,4 @@
 
-hexNumber = str(input("enter a hex character: "))
+hexNumber = input("enter a hex character: ")
 decimalNumber  = int(hexNumber, 16)
 print("The decimal value of", hexNumber, "is", decimalNumber)
